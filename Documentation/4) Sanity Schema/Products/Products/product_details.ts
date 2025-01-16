f5027ad8c27ef87c86ade7a65bb33fe9db1df86b
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2025-01-17 04:28:51 +0500

    Update product_details.ts

--- Documentation/4) Sanity Schema/Products/Products/product_details.ts
+++ Documentation/4) Sanity Schema/Products/Products/product_details.ts
@@ -1,41 +1,41 @@
 export default {
-	name : "productdetails",
-    type : "document",
-    title : "Product Details",
+    name : "productdetails",
+    type: "document",
+    title: "Product Details",
     fields : [
       {
-      	name  : "id",
-      	type  : "number",
-      	title : "Product ID",
+      	name : "id",
+      	type : "number",
+      	title: "Product ID",
       },
 
 
         {
-      	name  : "img",
-      	type  : "image",
-      	title : "Product Image",
+      	name : "img",
+      	type : "image",
+      	title: "Product Image",
       },
 
 
 
         {
-        name  : "title",
-        type  : "string",
-        title : "Product Title",
+        name : "title",
+        type : "string",
+        title: "Product Title",
       },
 
 
         {
-      	name  : "desc",
-      	type  : "string",
-      	title : "Product Description",
+      	name : "desc",
+      	type : "string",
+      	title: "Product Description",
       },
 
 
         {
-        name  : "price",
-        type  : "string",
-        title : "Product Price",
+        name : "price",
+        type : "string",
+        title: "Product Price",
       },
 
     ],
