3ed412443d059612c0e14e520ac922478ee4bff1
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2025-01-17 04:22:07 +0500

    Add files via upload

---
+++ Documentation/4) Sanity Schema/Products/Orders/customer_info.ts
@@ -0,0 +1,40 @@
+export default {
+	name : "customer_info",
+	type : "document",
+	title : "Customer_Info",
+	fields : [
+      
+      {
+      	name : "id",
+      	type : "number",
+      	title : "Customer ID",
+      },
+
+
+      {
+      	name : "name",
+      	type : "string",
+      	title : "Customer Name",
+      },
+
+
+      {
+      	name : "email",
+      	type : "string",
+      	title : "Customer Email",
+      },
+
+      {
+      	name : "phone",
+      	type : "string",
+      	title : "Customer PhoneNo",
+      },
+
+      {
+      	name : "shipp_addr",
+      	type : "string",
+      	title : "Shipping Address",
+      },
+
+	],
+}
---
+++ Documentation/4) Sanity Schema/Products/Orders/payment_status.ts
@@ -0,0 +1,34 @@
+export default {
+	name : "payment_status",
+	type : "document",
+	title : "Payment Status",
+	fields : [
+      
+      {
+      	name : "id",
+      	type : "number",
+      	title : "Payment ID",
+      },
+
+
+      {
+      	name : "amount",
+      	type : "string",
+      	title : "Amount",
+      },
+
+
+      {
+      	name : "date",
+      	type : "string",
+      	title : "Payment Date",
+      },
+
+      {
+      	name : "paymethod",
+      	type : "string",
+      	title : "Payment Method",
+      },
+
+	],
+}
