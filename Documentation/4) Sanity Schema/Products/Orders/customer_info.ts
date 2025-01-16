c4f84667e7627e4f29c84f70b326dadf51f279ab
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2025-01-17 04:31:00 +0500

    Update customer_info.ts

--- Documentation/4) Sanity Schema/Products/Orders/customer_info.ts
+++ Documentation/4) Sanity Schema/Products/Orders/customer_info.ts
@@ -1,39 +1,39 @@
 export default {
-	name : "customer_info",
-	type : "document",
-	title : "Customer_Info",
+	name: "customer_info",
+	type: "document",
+	title: "Customer_Info",
 	fields : [
       
       {
-      	name : "id",
-      	type : "number",
-      	title : "Customer ID",
+      	name: "id",
+      	type: "number",
+      	title: "Customer ID",
       },
 
 
       {
-      	name : "name",
-      	type : "string",
-      	title : "Customer Name",
+      	name: "name",
+      	type: "string",
+      	title: "Customer Name",
       },
 
 
       {
-      	name : "email",
-      	type : "string",
-      	title : "Customer Email",
+      	name: "email",
+      	type: "string",
+      	title: "Customer Email",
       },
 
       {
-      	name : "phone",
-      	type : "string",
-      	title : "Customer PhoneNo",
+      	name: "phone",
+      	type: "string",
+      	title: "Customer PhoneNo",
       },
 
       {
-      	name : "shipp_addr",
-      	type : "string",
-      	title : "Shipping Address",
+      	name: "shipp_addr",
+      	type: "string",
+      	title: "Shipping Address",
       },
 
 	],
