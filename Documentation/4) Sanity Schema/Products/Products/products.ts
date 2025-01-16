19969ca408ce4960decf3e85365fe7a4b5391fd9
Muzna Amir <159197516+MuznaZuberi@users.noreply.github.com>
2025-01-17 04:24:17 +0500

    Add files via upload

---
+++ Documentation/4) Sanity Schema/Products/Products/products.ts
@@ -0,0 +1,27 @@
+export default {
+	name : "product",
+    type : "document",
+    title : "Product",
+    fields : [
+      {
+      	name  : "img",
+      	type  : "image",
+      	title : "Product Image",
+      },
+
+
+        {
+      	name  : "title",
+      	type  : "string",
+      	title : "Product Title",
+      },
+
+
+        {
+      	name  : "price",
+      	type  : "string",
+      	title : "Product Price",
+      },
+
+    ],
+}
